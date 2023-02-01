97e56342445f8c462eb420eaa1c10efeb7c35416
Thiago Silva <thiago.tlsbomu@gmail.com>
2023-02-01 10:50:22 -0300

    Manual Rote

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ const http = require("http");
 
 http
     .createServer((request, response) => {
-        response.writeHead(200, { 'Content-Type': 'application/json'});
-        response.end(JSON.stringify({
+        let message = {
             data: 'Hello World!'
-        })) 
+        }
+        response.writeHead(200, { 'Content-Type': 'application/json'});
+        console.log(request.url)
+        switch(request.url) {
+            case '/produto' : 
+                message = {
+                    module: 'Produtos', 
+                    action: 'show'
+                }
+                break
+            case '/usuario' : 
+                message = {
+                    module: 'Usuários', 
+                    action: 'show'
+                }
+                break
+        }
+
+        response.end(JSON.stringify(message)) 
     })
     .listen(80, () => console.log("Servidor rodando na porta 80"))
